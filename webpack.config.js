56204dcdf45fa7756d0c5f118e5326d6608cf14c
Danilo Bürger <info@danilobuerger.de>
2016-10-12 00:13:04 +0200

    Removed unneeded externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
 var webpack = require('webpack')
 
 var config = {
-  externals: {
-    'react': {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    },
-    'redux': {
-      root: 'Redux',
-      commonjs2: 'redux',
-      commonjs: 'redux',
-      amd: 'redux'
-    },
-    'react-redux': {
-      root: 'ReactRedux',
-      commonjs2: 'react-redux',
-      commonjs: 'react-redux',
-      amd: 'react-redux'
-    }
-  },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
