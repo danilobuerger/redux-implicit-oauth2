6acac6aec2f51c815de5702b12d80a40f3ab9fff
Danilo Bürger <info@danilobuerger.de>
2017-09-21 23:22:34 +0200

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,13 @@ var webpack = require('webpack')
 
 var config = {
   module: {
-    loaders: [{ test: /\.js$/, loader: 'babel', exclude: /node_modules/ }]
+    rules: [{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }]
   },
   output: {
     library: 'ReduxImplicitOAuth2',
     libraryTarget: 'umd'
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
@@ -18,7 +17,18 @@ var config = {
 
 if (process.env.NODE_ENV === 'production') {
   config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false } })
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: false
+      },
+      mangle: {
+        screw_ie8: false
+      },
+      output: {
+        screw_ie8: false
+      }
+    })
   )
 }
 
